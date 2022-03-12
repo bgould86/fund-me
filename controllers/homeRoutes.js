ecfc639d8062a2fd2ccc38a515b2924e5514e6ec
bgould86 <bgould86@gmail.com>
2022-03-12 13:16:50 -0600

    able to log in and out

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,45 @@
 const router = require('express').Router();
 const { Project } = require('./../models');
+const withAuth = require('./../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-    const projectData = await Project.findAll();
-    const projects = projectData.map((project) => project.get({ plain: true }));
-    res.render('homepage', { projects });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+    try {
+        const projectData = await Project.findAll();
+        const projects = projectData.map((project) => project.get({ plain: true }));
+        res.render('homepage', { projects, loggedIn: req.session.logged_in });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Project }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
